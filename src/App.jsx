d7c85a54d10ef27ef8d94d58e2f3eb653cc6a036
Marcelo Castañeda <cesar.acm1@gmail.com>
2023-06-22 02:40:31 -0300

    03_hooks

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,16 @@
 import React from "react";
 // import HookState01 from "./components/UseState_01"
 // import HookState02 from "./components/UseState_02"
-import HookState03 from "./components/UseState_03"
+// import HookState03 from "./components/UseState_03"
+import HookState04 from "./components/UseState_04"
 export default ()=>{
     return ( 
         <>
         <h1> REACT HOOKS</h1>
         {/* <HookState01/> */}
         {/* <HookState02/> */}
-        <HookState03/> 
+        {/* <HookState03/>  */}
+        <HookState04/> 
         </>
     )
 }
---
+++ src/components/UseState_04a.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+export default ({unidades}) => {
+    const [valor, setValor]=useState(0)
+
+    function decrementar(){
+        setValor(old=>old-unidades);
+    }
+ 
+    function incrementar(){
+        setValor(old=>old+unidades);
+    }
+
+    return(
+        <>
+        <button onClick={decrementar}>-</button>
+        <h3>valor <strong>{valor}</strong></h3>
+        <button onClick={incrementar}>+</button>
+        </>
+    )
+}
