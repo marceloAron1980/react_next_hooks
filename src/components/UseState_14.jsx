dda7d55efc39ca0b392949cb35c138bf06ea702b
Marcelo Castañeda <cesar.acm1@gmail.com>
2023-07-05 14:29:40 -0300

    14_hooks

---
+++ src/components/UseState_14.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+export default () => {
+    const [text, setText]=useState("");
+    const [nomes, setNomes]=useState([]);
+
+    function inserir(){
+        setNomes(prev =>[...prev, text]);
+        setText('');
+        
+    }
+    return(
+        <>
+        <h3> Input Data - Collection </h3>
+        <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+        <button onClick={inserir}>Inserir</button>
+        <ul>
+            {
+                nomes.map((data, index)=>(
+                    <li key={index}>{data}</li>
+                ))
+            }
+        </ul>
+        </>
+)
+}
